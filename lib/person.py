d64cd857a23034b600ea1451a4b9926c7233fddd
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-14 21:24:17 -0500

    both deliverables achieved, working with properties in classes

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    def __init__(self,name = "Louis",job = "Sales"):
+        self.name = name
+        self.job = job
+
+    def name_getter(self):
+        return self._name
+
+    def name_setter(self,name):
+        if(type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def job_getter(self):
+        return self._job
+
+    def job_setter(self,job):
+        if (job in APPROVED_JOBS):
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(name_getter,name_setter,)
+    job = property(job_getter,job_setter,)
